e089a3b37eb70622618061fa433386ec0baf144b
Vijayrajvijay <vr104555@gmail.com>
2023-12-11 12:18:54 +0530

    created

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,11 @@ import studenRoutes from './student.js'
 
 const router = express.Router()
 
+router.get('/', (req,res)=>{
+
+    res.status(200).send(`<h1> Welcome to our Backend of Student Mentor assign API </h>`)
+})
+
 router.use('/mentor',mentorRoutes)
 router.use('/student',studenRoutes)
 
